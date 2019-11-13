12f79b9e6bdca351b75848223001ba35e06b7fea
hanqing <hanqingdyx@gmail.com>
2019-11-12 20:46:51 -0500

    init with hw4

---
+++ hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/Acrobot.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Acrobot.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/CartPole.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/CartPole.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/FourierBasis.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/FourierBasis.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/Gridworld.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Gridworld.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/MathUtils.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/MathUtils.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/MountainCar.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/MountainCar.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/QLearning.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/QLearning.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/Sarsa.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Sarsa.cpp.o"
+  "/Users/hanqing/workspace/RL/hw4/HW4/src/main.cpp" "/Users/hanqing/workspace/RL/hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw4/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CLion"
+  "CLion.pdb"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Acrobot.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/CartPole.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/FourierBasis.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Gridworld.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/MathUtils.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/MountainCar.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/QLearning.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/Sarsa.cpp.o"
+  "CMakeFiles/CLion.dir/Users/hanqing/workspace/RL/hw4/HW4/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
